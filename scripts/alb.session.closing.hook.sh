1cca79024c03d78fa21580b85403a906e4f37294
Dario La Porta <laportad@amazon.com>
2022-11-24 13:31:48 +0000

    Fixed syntax issues

--- scripts/alb.session.closing.hook.sh
+++ scripts/alb.session.closing.hook.sh
@@ -38,28 +38,36 @@ main() {
     local -- _target_host_filter=$([[ $_target_host == *\.* ]] && echo "$_target_host" || echo "$_target_host.*")
 
     aws help >/dev/null || _die "AWS Cli is not installed."
-
-    local -- _alb_arn=$(aws elbv2 describe-load-balancers --query "LoadBalancers[? DNSName == '$_alb_host'].LoadBalancerArn" --output text)
+    
+    local -- _alb_arn
+    _alb_arn=$(aws elbv2 describe-load-balancers --query "LoadBalancers[? DNSName == '$_alb_host'].LoadBalancerArn" --output text)
     [ -n "$_alb_arn" ] || _die "Unable to get ALB identifier for the ALB ($_alb_host)."
 
-    local -- _vpc_id=$(aws elbv2 describe-load-balancers --load-balancer-arns "$_alb_arn" \
+    local -- _vpc_id
+    _vpc_id=$(aws elbv2 describe-load-balancers --load-balancer-arns "$_alb_arn" \
         --query "LoadBalancers[].VpcId" --output text)
     [ -n "$_vpc_id" ] || _die "Unable to detect VPC of the ALB ($_alb_host)."
 
-    local -- _instance_id=$(aws ec2 describe-instances --filters "Name=private-dns-name,Values=$(echo $_target_host_filter | sed "s/-dcv//")" \
+    local -- _instance_id
+    _instance_id=$(aws ec2 describe-instances --filters "Name=private-dns-name,Values=$(echo $_target_host_filter | sed "s/-dcv//")" \
         --query "Reservations[].Instances[? VpcId == '$_vpc_id'].InstanceId" --output text)
     [ -n "$_instance_id" ] || _die "Unable to get Instance Id for the given Private DNS name filter ($_target_host_filter) in the VPC ($_vpc_id)."
 
-    local -- _listener_arn=$(aws elbv2 describe-listeners --load-balancer-arn "$_alb_arn" \
+    local -- _listener_arn
+    _listener_arn=$(aws elbv2 describe-listeners --load-balancer-arn "$_alb_arn" \
         --query 'Listeners[? Port == `'$_alb_port'`].ListenerArn' --output text)
     [ -n "$_listener_arn" ] || _die "Listener for port ($_alb_port) does not exist in the ALB ($_alb_host)."
 
-    local -- _target_group_name=$(printf "%s" "$_session_id" | tr -c 'a-zA-Z0-9' - | tr -d '-')
-    local -- _target_group_arn=$(aws elbv2 describe-target-groups --load-balancer-arn "$_alb_arn" \
+    local -- _target_group_name
+    _target_group_name=$(printf "%s" "$_session_id" | tr -c 'a-zA-Z0-9' - | tr -d '-')
+    
+    local -- _target_group_arn
+    _target_group_arn=$(aws elbv2 describe-target-groups --load-balancer-arn "$_alb_arn" \
         --query "TargetGroups[? TargetGroupName == '$_target_group_name'].TargetGroupArn" --output text)
     [ -n "$_target_group_arn" ] || _die "Unable to get Target Group ($_target_group_name)"
 
-    local -- _rule_arn=$(aws elbv2 describe-rules --listener-arn "$_listener_arn" \
+    local -- _rule_arn
+    _rule_arn=$(aws elbv2 describe-rules --listener-arn "$_listener_arn" \
         --query "Rules[? Actions[? TargetGroupArn == '$_target_group_arn']].RuleArn" --output text)
     [ -n "$_rule_arn" ] || _die "Unable to get Rule for Target Group ($_target_group_arn) in the Listener ($_listener_arn)."
 
@@ -69,8 +77,8 @@ main() {
     aws elbv2 delete-target-group --target-group-arn "$_target_group_arn" >/dev/null
     [ $? -eq 0 ] || _die "Unable to delete Target Group ($_target_group_arn)."
     
-    instance_id=$(aws ec2 describe-instances --filters Name=network-interface.addresses.private-ip-address,Values=${INTERACTIVE_SESSION_DCV2SM_EXECUTION_IP} --query "Reservations[].Instances[].InstanceId" --output text)
-    aws ec2 delete-tags --resources ${instance_id} --tags Key=UserID,Value=${EF_USER}
+    instance_id=$(aws ec2 describe-instances --filters Name=network-interface.addresses.private-ip-address,Values="${INTERACTIVE_SESSION_DCV2SM_EXECUTION_IP}" --query "Reservations[].Instances[].InstanceId" --output text)
+    aws ec2 delete-tags --resources "${instance_id}" --tags Key=UserID,Value="${EF_USER}"
 }
 if [ "$INTERACTIVE_SESSION_REMOTE" = "dcv2sm" ]; then
     main "$INTERACTIVE_SESSION_DELEGATESESSIONID" "$ALB_PUBLIC_DNS_NAME" "$ALB_PORT" "$INTERACTIVE_SESSION_EXECUTION_HOST" "$INTERACTIVE_SESSION_DCV2SM_EXECUTION_PORT" "$INTERACTIVE_SESSION_DCV2SM_WEBURLPATH"
--- scripts/alb.session.starting.hook.sh
+++ scripts/alb.session.starting.hook.sh
@@ -35,27 +35,35 @@ main() {
     [ -z "$_target_host" ] && _die "Missing input Target Host parameter."
     [ -z "$_target_port" ] && _die "Missing input Target Port parameter."
     [ -z "$_target_web_url_path" ] && _die "Missing input Target Web Url Path parameter."
-    local -- _target_host_filter=$([[ $_target_host == *\.* ]] && echo "$_target_host" || echo "$_target_host.*")
+    local -- _target_host_filter
+    _target_host_filter=$([[ $_target_host == *\.* ]] && echo "$_target_host" || echo "$_target_host.*")
 
     aws help >/dev/null || _die "AWS Cli is not installed."
 
-    local -- _alb_arn=$(aws elbv2 describe-load-balancers --query "LoadBalancers[? DNSName == '$_alb_host'].LoadBalancerArn" --output text)
+    local -- _alb_arn
+    _alb_arn=$(aws elbv2 describe-load-balancers --query "LoadBalancers[? DNSName == '$_alb_host'].LoadBalancerArn" --output text)
     [ -n "$_alb_arn" ] || _die "Unable to get ALB identifier for the ALB ($_alb_host)."
 
-    local -- _vpc_id=$(aws elbv2 describe-load-balancers --load-balancer-arns "$_alb_arn" \
+    local -- _vpc_id
+    _vpc_id=$(aws elbv2 describe-load-balancers --load-balancer-arns "$_alb_arn" \
         --query "LoadBalancers[].VpcId" --output text)
     [ -n "$_vpc_id" ] || _die "Unable to detect VPC of the ALB ($_alb_host)."
 
-    local -- _instance_id=$(aws ec2 describe-instances --filters "Name=private-dns-name,Values=$(echo $_target_host_filter | sed "s/-dcv//")" \
+    local -- _instance_id
+    _instance_id=$(aws ec2 describe-instances --filters "Name=private-dns-name,Values=$(echo "$_target_host_filter" | sed "s/-dcv//")" \
         --query "Reservations[].Instances[? VpcId == '$_vpc_id'].InstanceId" --output text)
     [ -n "$_instance_id" ] || _die "Unable to get Instance Id for the given Private DNS name filter ($_target_host_filter) in the VPC ($_vpc_id)."
 
-    local -- _listener_arn=$(aws elbv2 describe-listeners --load-balancer-arn "$_alb_arn" \
+    local -- _listener_arn
+    _listener_arn=$(aws elbv2 describe-listeners --load-balancer-arn "$_alb_arn" \
         --query 'Listeners[? Port == `'$_alb_port'`].ListenerArn' --output text)
     [ -n "$_listener_arn" ] || _die "Listener for port ($_alb_port) does not exist in the ALB ($_alb_host)."
 
-    local -- _target_group_name=$(printf "%s" "$_session_id" | tr -c 'a-zA-Z0-9' - | tr -d '-')
-    local -- _target_group_arn=$(aws elbv2 describe-target-groups --load-balancer-arn "$_alb_arn" \
+    local -- _target_group_name
+    _target_group_name=$(printf "%s" "$_session_id" | tr -c 'a-zA-Z0-9' - | tr -d '-')
+    
+    local -- _target_group_arn
+    _target_group_arn=$(aws elbv2 describe-target-groups --load-balancer-arn "$_alb_arn" \
         --query "TargetGroups[? TargetGroupName == '$_target_group_name'].TargetGroupArn" --output text)
     if [ -z "$_target_group_arn" ]; then
 
@@ -73,17 +81,22 @@ main() {
         --query "max(Rules[? Priority != 'default'].Priority.to_number(@))" --output text)
         [ -n "$_current_priority" ] || _current_priority=0
 
-        local -- _priority=$((_current_priority+1))
-        local -- _target_path="$_target_web_url_path*"
-        local -- _rule_arn=$(aws elbv2 create-rule --listener-arn "$_listener_arn" --priority "$_priority" \
-        --conditions Field=path-pattern,Values="$_target_path" --actions Type=forward,TargetGroupArn=$_target_group_arn \
+        local -- _priority
+        _priority=$((_current_priority+1))
+        
+        local -- _target_path
+        _target_path="$_target_web_url_path*"
+        
+        local -- _rule_arn
+        _rule_arn=$(aws elbv2 create-rule --listener-arn "$_listener_arn" --priority "$_priority" \
+        --conditions Field=path-pattern,Values="$_target_path" --actions Type=forward,TargetGroupArn="$_target_group_arn" \
         --query "Rules[0].RuleArn" --output text)
         [ -n "$_rule_arn" ] || _die "Unable to create Rule for the Listener ($_listener_arn), Target Group ($_target_group_arn) and target path ($_target_path)."
     fi
     sleep 10
 
-    instance_id=$(aws ec2 describe-instances --filters Name=network-interface.addresses.private-ip-address,Values=${INTERACTIVE_SESSION_DCV2SM_EXECUTION_IP} --query "Reservations[].Instances[].InstanceId" --output text)
-    aws ec2 create-tags --resources ${instance_id} --tags Key=UserID,Value=${EF_USER}
+    instance_id=$(aws ec2 describe-instances --filters Name=network-interface.addresses.private-ip-address,Values="${INTERACTIVE_SESSION_DCV2SM_EXECUTION_IP}" --query "Reservations[].Instances[].InstanceId" --output text)
+    aws ec2 create-tags --resources "${instance_id}" --tags Key=UserID,Value="${EF_USER}"
 
     export INTERACTIVE_SESSION_TARGET_HOST="$_alb_host"
     export INTERACTIVE_SESSION_TARGET_PORT="$_alb_port"
